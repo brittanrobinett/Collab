259bf86171346f6b1e64e9d92893a75f24efbc60
Micah Witthaus <micah.witthaus@gmail.com>
2019-07-05 15:18:22 -0700

    created seperate stylesheet as well as added some server code to fix the localhost 404 error

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const io = require('socket.io')(3000)
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+users = [];
+connections = [];
 
-io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
-})
+server.listen(process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnected Sockets
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    // Send Message
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', { msg: data });
+    });
+});
